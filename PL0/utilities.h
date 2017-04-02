bb80b25eb64082fb9fe584f4c991227e90b9dcc9
FlorianFusseder-Desktop <florianfueder@yahoo.de>
2017-04-02 19:07:46 +0200

    Inital commit

---
+++ PL0/utilities.h
@@ -0,0 +1,12 @@
+//==============================================================================
+//  utilities.h:   declarations for the utility functions
+//  last change:   15.10.98/Kp
+//==============================================================================
+#if !defined (__utilities_h)
+#define __utilities_h
+
+// prototypes for the utility functions
+void getInputFile(char *inFileName, fstream& inFile);
+void weiter(void);
+
+#endif
